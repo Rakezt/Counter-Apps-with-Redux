8ab9e5c0a75475d4e7e080b104f47eef5f7f2095
L Rakesh Singh <110081692+Rakezt@users.noreply.github.com>
2024-02-23 12:51:24 +0530

    update middleware

--- src/component/Store.js
+++ src/component/Store.js
@@ -1,5 +1,6 @@
-import { createStore } from "redux";
+import { applyMiddleware, createStore } from "redux";
 import CounterReducer from "./CounterReducer";
+import loggerMiddleware from "./LoggerMiddleware";
 
-const store = createStore(CounterReducer);
+const store = createStore(CounterReducer, applyMiddleware(loggerMiddleware));
 export default store;
